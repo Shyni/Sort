e2ddbacb6ffa82512bbacc8bdcd180583e6e61f3
Shyni Joseph <shynijoseph82@gmail.com>
2013-08-03 17:07:50 +0530

    Create DataBean.java

---
+++ DataBean.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author User
+ */
+class DataBean {
+    String level;
+    String name;
+    String id;
+    
+    public String getLevel(){
+        return level;
+    }
+    
+    public void setLevel(String level){
+        this.level=level;
+    }
+    
+      public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name=name;
+    }
+    
+      public String getId(){
+        return id;
+    }
+    
+    public void setId(String id){
+        this.id=id;
+    }
+    
+}
