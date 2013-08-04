f29cba7c05e36538db83d76360c91f45b5e518b2
Shyni Joseph <shynijoseph82@gmail.com>
2013-08-04 15:59:25 +0530

    Update SortCreator.java

--- SortCreator.java
+++ SortCreator.java
@@ -1,87 +1,74 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication1;
+package sort;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Set;
 
-/**
- *
- * @author User
- */
+
 public class SortCreator {
-        static List dataList = new ArrayList();
-        static String str=null;
-        static BufferedReader fr=null;
-        static Properties prop = new Properties();
-        static Map objMap = new HashMap();
-        static int startIndex=4;
     
     public static void main(String[] args){
-        SortCreator objSortCreator = new SortCreator();
-       
-        try {     
-            fr=new BufferedReader(new FileReader("C:\\Test\\Data.txt"));
-            while((str=fr.readLine())!=null){              
-                String[] data=str.split("\\|");
-                DataBean db = new DataBean();
-                db.setLevel(data[0]);
-                db.setName(data[1]);
-                db.setId(data[2]);
-                dataList.add(db);
-                if(data[2].equals("123")){
-                    objMap.put(startIndex, db);
-                }
-            }            
-        } catch (IOException ex) {
-            Logger.getLogger(SortCreator.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        objSortCreator.sortData();
+            SortCreator creator = new SortCreator();
+        List<DataBean> dataList = FileLoader.getDefaultSortedData();
+        Map<String, List<String>> userPrefMap = FileLoader.getUserPref();
+        System.out.println("userPrefMap : "+userPrefMap); 
+        dataList = creator.getUserPrefferedSortedData(dataList,userPrefMap);
+        for (int i = 0; i < dataList.size(); i++) {
+            //DataBean dataBean = dataList.get(i);
+            System.out.println(dataList.get(i));            
+        }       
     }
-
-    private void sortData(){
-        System.out.println("sortData : dataList.size() : "+dataList.size());
-        System.out.println("sortData : objMap.size() : "+objMap.size());
-        DataBean db = (DataBean)objMap.get(startIndex);
-        
-            try {
-                prop.load(new FileReader("C:\\Test\\UserPref.txt"));
-            } catch (IOException ex) {
-                Logger.getLogger(SortCreator.class.getName()).log(Level.SEVERE, null, ex);
-            }
-         String parent=prop.getProperty("123");
-         String[] dataArr = parent.split("\\|");
-        List childList = null;
-         for (int i = 0; i < dataArr.length; i++) {   
-            childList=new ArrayList();
-            System.out.println("sortData : user pref : "+dataArr[i]);            
+	private List<DataBean> getUserPrefferedSortedData(List<DataBean> dataList, Map<String, List<String>> userPrefMap) {
+		Set<String> userPrefKeys = userPrefMap.keySet();
+		Map<String, Integer> userPrefIndex = getIndexOfuserPrefKey(dataList,userPrefKeys);
+		Map<String, List<DataBean>> level2ListMap = getAllLevel2List(dataList);
+		
+		for (String userPrefKey : userPrefKeys) {
+			List<String> level2Keys = userPrefMap.get(userPrefKey);
+			int index = userPrefIndex.get(userPrefKey)+1;
+			 for(String level2Key : level2Keys){
+				    List<DataBean>	leve2List = level2ListMap.get(level2Key);
+				    if(leve2List!= null){
+						dataList.removeAll(leve2List);
+						dataList.addAll(index,leve2List);
+						index += leve2List.size();
+				    }
+			 }
+		}		
+		return dataList;
         }
+	private Map<String, List<DataBean>> getAllLevel2List(List<DataBean> dataList) {
+		Map<String, List<DataBean>> Level2PrefListMap = new HashMap<String, List<DataBean>>();
+		List<DataBean> level2List = null;
+		String FordataBeanId = null;
+		for(DataBean dataBean :dataList){
+				
+			if(dataBean.getLevel().equalsIgnoreCase("1")){
+				continue;
+			}
+			
+			if(dataBean.getLevel().equalsIgnoreCase("2")){
+				FordataBeanId = dataBean.getId();
+				level2List = new ArrayList<DataBean>();
+			}
+			
+			level2List.add(dataBean);
+			Level2PrefListMap.put(FordataBeanId,level2List);
+		}
+                System.out.println("getAllLevel2List : Level2PrefListMap : "+Level2PrefListMap);
+		return Level2PrefListMap;
+	}
 
-         for (int i = startIndex+1; i < dataList.size(); i++) {      
-            DataBean dbean = (DataBean)dataList.get(i);
-         
-            if(dbean.getLevel().equals("1")){                            
-                break;
-            }
-          
-            if(!dbean.getLevel().equals("1") && dbean.getLevel().compareTo("2")>=0){
-                childList.add(dbean);
-                
-                System.out.println("i : "+i+" size() : "+childList.size()+" level : "+dbean.getLevel()+" getName() : "+dbean.getName());
-            }           
-        }           
-    }
-    
-    
+
+	private static Map<String, Integer> getIndexOfuserPrefKey(List<DataBean> dataList, Set<String> userPrefKeys) {
+		Map<String, Integer> userPrefKeyIndex = new HashMap<String, Integer>();
+		for(int i = 0;i< dataList.size();i++)
+			if(userPrefKeys.contains(dataList.get(i).getId())) userPrefKeyIndex.put(dataList.get(i).getId(), i);
+                System.out.println("getIndexOfuserPrefKey : userPrefKeyIndex : "+userPrefKeyIndex);
+		return userPrefKeyIndex;
+	}
+
     
 }
