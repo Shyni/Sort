b2bc97ea34f6c79460fbeace4b174572346b0eb4
Shyni Joseph <shynijoseph82@gmail.com>
2013-08-14 16:27:10 +0530

    Update TreeMapExample.java

--- TreeMapExample.java
+++ TreeMapExample.java
@@ -36,7 +36,7 @@ public class TreeMapExample {
         //create an instance of TreeMap to Store Phone Number
         Map testTreeMap = new TreeMap();
         
-        /*try {
+        try {
             prop.load(new FileInputStream("D:/testProp.txt"));
         } catch (IOException ex) {
             Logger.getLogger(TreeMapExample.class.getName()).log(Level.SEVERE, null, ex);
@@ -51,30 +51,21 @@ public class TreeMapExample {
         while(i.hasNext())
         {
                 String key = (( Map.Entry )i.next()).getKey().toString();
-                System.out.println("Key = " + key);
-                //testTreeMap.put(key.getKey().trim(), prop.getProperty(key.getKey()).replace('"',(char) 32).trim());
-                testTreeMap.put(key, prop.getProperty(key));
-        }  */    
+               // System.out.println("Key = " + key+" prop.getProperty(key) : "+prop.getProperty(key));
+                testTreeMap.put(key.trim(), prop.getProperty(key).replace('"',(char) 32).trim());
+               // testTreeMap.put(key, prop.getProperty(key));
+        }     
  
         //Populate example map with values
-        testTreeMap.put("AK1", 258963147);
-        testTreeMap.put("AK5", 5874569);
-        testTreeMap.put("AK3", 58963147);
-        testTreeMap.put("AK6", 523658);
-        testTreeMap.put("AK5", 5333658);
-        testTreeMap.put("AK4", 4566878);
-        testTreeMap.put("AK2", 852369);
-         System.out.println(testTreeMap);
          
-        /*testTreeMap.put("AK1", "Code");
-        testTreeMap.put("AK5", "CINA-");
+      /*  testTreeMap.put("AK1", "Code");
+        testTreeMap.put("AK7", "CINA-");
         testTreeMap.put("AK3", "Adoption Rules");
         testTreeMap.put("AK6", "ok1");
         testTreeMap.put("AK5", "ok2");
         testTreeMap.put("AK4", "ok3");
-        testTreeMap.put("AK2", "ok4");
+        testTreeMap.put("AK2", "ok4"); */        
          
-         */
         for (Iterator it = testTreeMap.entrySet().iterator(); it.hasNext();) {
             Map.Entry entry = (Map.Entry) it.next();
             System.out.println("Key = " + entry.getKey() + ", Value = " +
@@ -83,3 +74,26 @@ public class TreeMapExample {
         System.out.println(testTreeMap); 
     }
 }
+/*
+ Key = AK1, Value = Code
+Key = AK2, Value = ok4
+Key = AK3, Value = Adoption Rules
+Key = AK4, Value = ok3
+Key = AK5, Value = ok2
+Key = AK6, Value = ok1
+Key = AK7, Value = CINA-
+{AK1=Code, AK2=ok4, AK3=Adoption Rules, AK4=ok3, AK5=ok2, AK6=ok1, AK7=CINA-}
+* 
+* Key = AK1, Value = "Code"
+Key = AK10, Value = "CINA-"
+Key = AK2, Value = "State Constitution"
+Key = AK3, Value = "Rules of Civil Procedure"
+Key = AK4, Value = "Rules of Criminal Procedure"
+Key = AK5, Value = "Rules of Evidence"
+Key = AK6, Value = "Rules of Appellate Procedure"
+Key = AK7, Value = "District Court Rules of Civil Procedure"
+Key = AK8, Value = "District Court Rules of Criminal Procedure"
+Key = AK9, Value = "Delinquency Rules"
+{AK1="Code", AK10="CINA-", AK2="State Constitution", AK3="Rules of Civil Procedure", AK4="Rules of Criminal Procedure", AK5="Rules of Evidence", AK6="Rules of Appellate Procedure", AK7="District Court Rules of Civil Procedure", AK8="District Court Rules of Criminal Procedure", AK9="Delinquency Rules"}
+
+ */
