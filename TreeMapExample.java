9a322974aaaa343da15aa01bec4be36fb6c9a0ea
Shyni Joseph <shynijoseph82@gmail.com>
2013-08-14 20:38:07 +0530

    Update TreeMapExample.java

--- TreeMapExample.java
+++ TreeMapExample.java
@@ -2,15 +2,15 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication1;
+package sort;
 
 /**
  *
- * @author shyni_joseph
+ * @author User
  */
-
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -34,10 +34,20 @@ public class TreeMapExample {
  
     public static void main(String[] args) {
         //create an instance of TreeMap to Store Phone Number
-        Map testTreeMap = new TreeMap();
+          Map<String, String> testTreeMap = new TreeMap<String, String>(new Comparator<String>() {
+
+			@Override
+			public int compare(String str1, String str2) {
+				str1 = str1.substring(2,str1.length());
+				str2 = str2.substring(2,str2.length());
+				Integer integer1 = Integer.valueOf(str1);
+				Integer integer2 = Integer.valueOf(str2);
+				return integer1.compareTo(integer2);
+			}
+		});
         
         try {
-            prop.load(new FileInputStream("D:/testProp.txt"));
+            prop.load(new FileInputStream("C:\\Users\\User\\Documents\\NetBeansProjects\\JavaApplication1\\src\\sort\\testProp.properties"));
         } catch (IOException ex) {
             Logger.getLogger(TreeMapExample.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -72,31 +82,3 @@ public class TreeMapExample {
         System.out.println(testTreeMap); 
     }
 }
-/*
- Key = AK1, Value = Code
-Key = AK2, Value = ok4
-Key = AK3, Value = Adoption Rules
-Key = AK4, Value = ok3
-Key = AK5, Value = ok2
-Key = AK6, Value = ok1
-Key = AK7, Value = CINA-
-{AK1=Code, AK2=ok4, AK3=Adoption Rules, AK4=ok3, AK5=ok2, AK6=ok1, AK7=CINA-}
-* 
-* Key = AK1, Value = Code
-Key = AK10, Value = Cina
-Key = AK11, Value = Cina1
-Key = AK12, Value = Cina2
-Key = AK13, Value = Cina3
-Key = AK14, Value = Cina4
-Key = AK15, Value = Cina5
-Key = AK2, Value = State Constitution
-Key = AK3, Value = Rules of Civil Procedure
-Key = AK4, Value = Rules of Criminal Procedure
-Key = AK5, Value = Rules of Evidence
-Key = AK6, Value = Rules of Appellate Procedure
-Key = AK7, Value = District Court Rules of Civil Procedure
-Key = AK8, Value = District Court Rules of Criminal Procedure
-Key = AK9, Value = Delinquency Rules
-{AK1=Code, AK10=Cina, AK11=Cina1, AK12=Cina2, AK13=Cina3, AK14=Cina4, AK15=Cina5, AK2=State Constitution, AK3=Rules of Civil Procedure, AK4=Rules of Criminal Procedure, AK5=Rules of Evidence, AK6=Rules of Appellate Procedure, AK7=District Court Rules of Civil Procedure, AK8=District Court Rules of Criminal Procedure, AK9=Delinquency Rules}
-
- */
