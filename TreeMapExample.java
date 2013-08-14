72fd4df8f2f687d2bd2b9af80452009712dd0854
Shyni Joseph <shynijoseph82@gmail.com>
2013-08-14 16:56:15 +0530

    Update TreeMapExample.java

--- TreeMapExample.java
+++ TreeMapExample.java
@@ -50,10 +50,8 @@ public class TreeMapExample {
 
         while(i.hasNext())
         {
-                String key = (( Map.Entry )i.next()).getKey().toString();
-               // System.out.println("Key = " + key+" prop.getProperty(key) : "+prop.getProperty(key));
-                testTreeMap.put(key.trim(), prop.getProperty(key).replace('"',(char) 32).trim());
-               // testTreeMap.put(key, prop.getProperty(key));
+                String key = (( Map.Entry )i.next()).getKey().toString();              
+                testTreeMap.put(key.replace('"',(char) 32).trim(), prop.getProperty(key).replace('"',(char) 32).trim());             
         }     
  
         //Populate example map with values
@@ -84,16 +82,21 @@ Key = AK6, Value = ok1
 Key = AK7, Value = CINA-
 {AK1=Code, AK2=ok4, AK3=Adoption Rules, AK4=ok3, AK5=ok2, AK6=ok1, AK7=CINA-}
 * 
-* Key = AK1, Value = "Code"
-Key = AK10, Value = "CINA-"
-Key = AK2, Value = "State Constitution"
-Key = AK3, Value = "Rules of Civil Procedure"
-Key = AK4, Value = "Rules of Criminal Procedure"
-Key = AK5, Value = "Rules of Evidence"
-Key = AK6, Value = "Rules of Appellate Procedure"
-Key = AK7, Value = "District Court Rules of Civil Procedure"
-Key = AK8, Value = "District Court Rules of Criminal Procedure"
-Key = AK9, Value = "Delinquency Rules"
-{AK1="Code", AK10="CINA-", AK2="State Constitution", AK3="Rules of Civil Procedure", AK4="Rules of Criminal Procedure", AK5="Rules of Evidence", AK6="Rules of Appellate Procedure", AK7="District Court Rules of Civil Procedure", AK8="District Court Rules of Criminal Procedure", AK9="Delinquency Rules"}
+* Key = AK1, Value = Code
+Key = AK10, Value = Cina
+Key = AK11, Value = Cina1
+Key = AK12, Value = Cina2
+Key = AK13, Value = Cina3
+Key = AK14, Value = Cina4
+Key = AK15, Value = Cina5
+Key = AK2, Value = State Constitution
+Key = AK3, Value = Rules of Civil Procedure
+Key = AK4, Value = Rules of Criminal Procedure
+Key = AK5, Value = Rules of Evidence
+Key = AK6, Value = Rules of Appellate Procedure
+Key = AK7, Value = District Court Rules of Civil Procedure
+Key = AK8, Value = District Court Rules of Criminal Procedure
+Key = AK9, Value = Delinquency Rules
+{AK1=Code, AK10=Cina, AK11=Cina1, AK12=Cina2, AK13=Cina3, AK14=Cina4, AK15=Cina5, AK2=State Constitution, AK3=Rules of Civil Procedure, AK4=Rules of Criminal Procedure, AK5=Rules of Evidence, AK6=Rules of Appellate Procedure, AK7=District Court Rules of Civil Procedure, AK8=District Court Rules of Criminal Procedure, AK9=Delinquency Rules}
 
  */
